11ed7dd4de185ef0af2a8cdc8765b1d00c530e45
Alexandre Junior <jasj_jr@hotmail.com>
2020-06-10 10:07:06 -0300

    Authenticating user

---
+++ src/services/CreateSessionService.ts
@@ -0,0 +1,47 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class CreateSessionService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, 'e67a2b8ada4f5007ea5acaf80fdf8545', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default CreateSessionService;
