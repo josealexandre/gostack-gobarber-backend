8673fb15a9e244a823dcc38c5b32495d96c01626
Alexandre Junior <jasj_jr@hotmail.com>
2020-06-14 11:50:24 -0300

    Check authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e67a2b8ada4f5007ea5acaf80fdf8545',
+    expiresIn: '1d',
+  },
+};
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +33,11 @@ class CreateSessionService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, 'e67a2b8ada4f5007ea5acaf80fdf8545', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
