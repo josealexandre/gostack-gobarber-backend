ae0eb470f8be04b3f2ad0204ce6813256b1814a3
Alexandre Junior <jasj_jr@hotmail.com>
2020-06-15 10:00:10 -0300

    Update user avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
 
+const tmpDirectory = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tmpDirectory,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpDirectory,
     filename(request, file, callback) {
       const filenHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${filenHash}-${file.originalname}`;
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import User from '../models/User';
+
+import uploadConfig from '../config/upload';
+
+interface Request {
+  userId: string;
+  avatarFilnename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ userId, avatarFilnename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(userId);
+
+    if (!user) {
+      throw new Error('Only authentiicated users can change avatar');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilnename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
